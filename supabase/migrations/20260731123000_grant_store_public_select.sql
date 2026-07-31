20dfd7ee451f498316c0746a5308cc1acc4c6806
luiz henrique <santosluishenrique96@gmail.com>
2026-07-31 11:13:28 -0300

    fix permissao publica da loja no supabase

---
+++ supabase/migrations/20260731123000_grant_store_public_select.sql
@@ -0,0 +1,5 @@
+GRANT SELECT ON TABLE public.products TO anon;
+GRANT SELECT ON TABLE public.products TO authenticated;
+
+GRANT SELECT ON TABLE public.store_settings TO anon;
+GRANT SELECT ON TABLE public.store_settings TO authenticated;
